91990a8d98d42cf556f8d1668a57ce39f2cd9f6c
matthewhasan <matthewhasan1218@gmail.com>
2021-03-10 10:27:19 -0600

    Login Styled

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -6,25 +6,41 @@ import LogoutButton from './auth/LogoutButton';
 const NavBar = ({ setAuthenticated }) => {
   return (
     <nav className="relative flex flex-wrap items-center justify-between navbar-expand-lg bg-accentLight mb-3 bg-gray-200">
-      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between list-none">
-        <div list-none="true" className="px-2 py-3">
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
+      <div className="container">
+        <div className=" px-4 mx-auto flex flex-wrap items-center justify-between list-none">
+
+        <div>
+          <div list-none="true" className="px-2 py-3">
+            <NavLink to="/" exact={true} activeClassName="active">
+              Home
+            </NavLink>
+          </div>
         </div>
-        <div list-none="true" className="px-2 py-3">
-          <NavLink to="/rosters" exact={true} activeClassName="active">
-            Rosters
-          </NavLink>
+
+        <div>
+          <div list-none="true" className="px-2 py-3">
+            <NavLink to="/rosters" exact={true} activeClassName="active">
+              Rosters
+            </NavLink>
+          </div>
         </div>
-        <div list-none="true" className="px-2 py-3">
-          <NavLink to="/members" exact={true} activeClassName="active">
-            Database
-          </NavLink>
+
+        <div>
+          <div list-none="true" className="px-2 py-3">
+            <NavLink to="/members" exact={true} activeClassName="active">
+              Database
+            </NavLink>
+          </div>
         </div>
-        <div className="px-2 py-3">
-          <LogoutButton setAuthenticated={setAuthenticated} />
+
+        <div>
+          <div className="px-2 py-3">
+            <LogoutButton setAuthenticated={setAuthenticated} />
+          </div>
         </div>
+        </div>
+
+
       </div>
     </nav>
   );
--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -30,15 +30,18 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
   }
 
   return (
-    <form className="bg-black" onSubmit={onLogin}>
+    <>
+    <form className="border-black m-auto flex flex-col w-6/12" onSubmit={onLogin}>
       <div>
         {errors.map((error) => (
           <div>{error}</div>
         ))}
+        <h1 className="bg-gray-200 border-black p-4 text-center w-full"> Login </h1>
       </div>
-      <div>
-        <label htmlFor="email">Email</label>
+      <div className="flex flex-col p-2">
+        <label className="text-left p-1 text-left" htmlFor="email">Email</label>
         <input
+          className="bg-gray-100"
           name="email"
           type="text"
           placeholder="Email"
@@ -46,18 +49,23 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
           onChange={updateEmail}
         />
       </div>
-      <div>
-        <label htmlFor="password">Password</label>
+      <div className="flex flex-col p-2">
+        <label className="text-left p-1 text-left" htmlFor="password">Password</label>
         <input
+          className="bg-gray-100"
           name="password"
           type="password"
           placeholder="Password"
           value={password}
           onChange={updatePassword}
         />
-        <button type="submit">Login</button>
+        <button className="bg-green-700 w-6/12 self-center p-1 m-4 hover:bg-blue-300 hover:text-yellow-300 rounded" type="submit">Login</button>
+      </div>
+      <div className="p-3">
+        <a classNane="text-blue-500" href="/sign-up">I don't have an account</a>
       </div>
     </form>
+    </>
   );
 };
 
