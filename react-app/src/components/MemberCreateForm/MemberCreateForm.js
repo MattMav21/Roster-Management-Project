1e6e56037b36b4916830374dcb8d3ad189698aa7
matthewhasan <matthewhasan1218@gmail.com>
2021-03-10 08:58:35 -0600

    styling another form

--- react-app/src/components/MemberCreateForm/MemberCreateForm.js
+++ react-app/src/components/MemberCreateForm/MemberCreateForm.js
@@ -23,26 +23,32 @@ const MemberCreateForm = () => {
     }
 
     return (
-        <div className="container p-3 m-auto bg-gray-200 rounded">
-            <form className="self-center m-auto" method="POST" action="/members/create/new" onSubmit={onSubmit}>
-                <h1>Member Create Form</h1>
-                <div>
-                    <h1>Name</h1>
-                    <input
-                        type="text"
-                        onChange={(e) => setName(e.target.value)}
-                        value={name}
-                    >
-                    </input>
-                    <h1>Notes</h1>
-                    <textarea
-                        type="text"
-                        onChange={(e) => setNotes(e.target.value)}
-                        value={notes}
-                    />
-                </div>
-                <button type="submit" disabled={name == "" ? true : false} className="bg-black text-white p-2 rounded hover:bg-white hover:text-black disabled:opacity-50">Submit</button>
-            </form>
+        <div className="block p-3 text-center bg-gray-600">
+            <div className="container p-3 m-auto bg-gray-200 rounded">
+                <form className="self-center m-auto" method="POST" action="/members/create/new" onSubmit={onSubmit}>
+                    <h1 className="bg-black text-white p-3">Add a new member to your database!</h1>
+                    <div>
+                        <div className="flex flex-col p-2">
+                            <h1 className="text-left p-1">Name: </h1>
+                            <input
+                                type="text"
+                                onChange={(e) => setName(e.target.value)}
+                                value={name}
+                            >
+                            </input>
+                        </div>
+                        <div className="flex flex-col p-2">
+                            <h1 className="p-1 text-left">Notes:</h1>
+                            <textarea
+                                type="text"
+                                onChange={(e) => setNotes(e.target.value)}
+                                value={notes}
+                            />
+                        </div>
+                    </div>
+                    <button type="submit" disabled={name == "" ? true : false} className="bg-black text-white p-2 rounded hover:bg-white hover:text-black disabled:opacity-50">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
