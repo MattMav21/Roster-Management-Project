64430b95431052ad6885f566fc51cf9e5c0f7f24
matthewhasan <matthewhasan1218@gmail.com>
2021-03-11 11:07:55 -0600

    started styling landing page

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -3,24 +3,26 @@ import { Link, Redirect } from "react-router-dom"
 import * as memberActions from "../../store/member"
 import { useDispatch, useSelector } from 'react-redux';
 // import * as UserActions from '../../store/'
+import "./LandingPage.css"
+
 
 const LandingPage = ({authenticated, setAuthenticated}) => {
     const dispatch = useDispatch();
-
     if (authenticated) {
         return <Redirect to="/home" />;
     }
 
     return (
-        <div className="splash-page container flex flex-col w-full p-12">
-            <div className="border-4 pb-32">
-                <div className="bg-blue-800 text-white p-10 rounded">
+        <>
+        <div className="splash-page flex flex-col h-screen w-screen">
+            <div className="container absolute m-auto bg-gray-800 top-20 w-6/12 left-52">
+                <div className="bg-blue-800 text-white rounded">
                     <h1 className="font-bold text-center">ROSTER MANAGEMENT</h1>
                 </div>
                     <h2 className="text-center">Manage your roster today!</h2>
             </div>
         </div>
-        // <div className="container flex m-auto flex-nowrap">
+        {/* // <div className="container flex m-auto flex-nowrap">
         //     <div className="container flex p-10 m-auto space-x-10 m-auto justify-around flex-nowrap">
         //         <Link to="/rosters/create" className="border-black bg-gray-500 p-5 rounded cursor-pointer">
         //             Create a new roster!
@@ -32,7 +34,8 @@ const LandingPage = ({authenticated, setAuthenticated}) => {
         //             Assign a member to your roster!
         //             </Link>
         //     </div>
-        // </div>
+        // </div> */}
+        </>
     )
 }
 
